e671eb5c16c1a2d253d337cd1e0fdbb52e53e277
daviswestbrook <dcw8ssq@virginia.edu>
2023-05-28 16:58:51 -0500

    Adding starting modules

---
+++ Message.py
@@ -0,0 +1,31 @@
+
+
+def Message:
+    __init__(self, text, sender_ID):
+        self.text = text
+        self.sender_ID = sender_ID
+        self.timestamp = timestamp
+
+def Group:
+    __init__(self, group_ID):
+        self.group_ID = group_ID
+
+def get_message_stream():
+    pass
+
+def get_message_stream_by_group(messages, group_ID):
+    pass
+
+def get_groups():
+    pass
+
+def specify_group():
+    pass
+
+
+
+
+
+
+        
+    
---
+++ main.py
@@ -0,0 +1,18 @@
+import Message
+
+# Get messages
+
+messages = Message.get_message_stream()
+
+# Find groups
+groups = Message.get_groups()
+
+# Ask user to specify group
+group_ID = Message.specify_groups()
+
+# Get handles in the group
+
+# Clean up texters with user help
+
+# Make report
+
