098434b95b1426c43ec0296c1d33bcc90582d1f3
daviswestbrook <dcw8ssq@virginia.edu>
2023-06-02 13:16:55 -0500

    set up tests

---
+++ tests/test_group.py
@@ -0,0 +1,15 @@
+def test_write_to_csv():
+    #TODO 
+    assert 1==1
+def test_get_groups():
+    #TODO 
+    assert 1==1
+def test_specify_group():
+    #TODO 
+    assert 1==1
+def test_make_texters():
+    #TODO 
+    assert 1==1
+def test_get_handles():
+    #TODO 
+    assert 1==1
---
+++ tests/test_message.py
@@ -0,0 +1,18 @@
+def get_message_stream():
+    #TODO  
+    assert 1==1
+def get_message_stream_by_group(): 
+    #TODO 
+    assert 1==1
+def add_object_column_to_stream():
+    #TODO 
+    assert 1==1
+def add_reaction_columns():
+    #TODO  
+    assert 1==1
+def get_message_stream_from_csv():
+    #TODO 
+    assert 1==1
+def generate_reactors():
+    #TODO 
+    assert 1==1
